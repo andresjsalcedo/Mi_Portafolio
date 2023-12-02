7c6bfc0d91d731a871a5b8c5264ba30ab57c5cc2
andresjsalcedo <andresjsalcedo@soy.sena.edu.co>
2023-12-01 20:49:00 -0500

    se concluyo la pagina web

---
+++ script.js
@@ -0,0 +1,6 @@
+function toogleMenu () {
+    const menu = document.querySelector("Menu-links");
+    const icon = document.querySelector("hamburger-icon");
+    menu.classList.toggle ("open");
+    icon.classList.toggle ("open");
+}
